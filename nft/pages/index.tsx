d179ed1bedfb9f8d3d7149bc5168f65892d65c1d
PoMaMa69 <saswatapatra15@gmail.com>
2023-11-05 12:02:50 +0530

    [ADD] Base Layout till 2nd lecture

---
+++ nft/pages/index.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import BaseLayout from '@/components/layout/BaseLayout'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <BaseLayout>
+    <div>Hello world</div>
+    <div>Hello world</div>
+    </BaseLayout>
+  )
+}
