824da81b639ca18ad6c4b49d13b5aaee6ac1a425
PoMaMa69 <saswatapatra15@gmail.com>
2023-12-03 23:06:43 +0530

    Lec7 [ADD] active_link, create.tsx,profile.tsx

--- nft/components/navbar/index.tsx
+++ nft/components/navbar/index.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 // import logo from '../public/images/small-eth.webp';
 import logo from "../../public/images/small-eth.webp";
 import Link from "next/link";
-
+import ActiveLink from "../link";
 const navigation = [
   { name: "Marketplace", href: "/", current: true },
   { name: "Create NFT", href: "/nft/create", current: false },
@@ -41,19 +41,16 @@ export default function Navbar() {
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <Link
+                      <ActiveLink
                         key={item.name}
                         href={item.href}
-                        className={classNames(
-                          item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "rounded-md px-3 py-2 text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
+                        activeClass="bg-gray-900 text-white"
+                        
+                        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                        aria-current={item.current ? 'page' : undefined}
                       >
-                        {item.name}
-                      </Link>
+                        <>{item.name}</>
+                      </ActiveLink>
                     ))}
                   </div>
                 </div>
