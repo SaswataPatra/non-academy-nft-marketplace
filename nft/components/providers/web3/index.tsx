fe06cd89e5a2f449860e5d420b21663932e34549
PoMaMa69 <saswatapatra15@gmail.com>
2023-12-10 12:30:08 +0530

    lec_10 [ADD] providers and signers

--- nft/components/providers/web3/index.tsx
+++ nft/components/providers/web3/index.tsx
@@ -1,10 +1,29 @@
-import { FunctionComponent, createContext, useContext, useState } from "react";
+import { FunctionComponent, createContext, useContext, useEffect, useState } from "react";
+import { MetaMaskInpageProvider } from "@metamask/providers";
+import { Web3State, createDefaultState } from "./utils";
+import { ethers } from "ethers";
 
-
-const Web3Context = createContext<any>(null)
+const Web3Context = createContext<Web3State>(createDefaultState())
 
 const Web3Provider : FunctionComponent<any> =({children}) =>{
-    const [web3Api, setweb3Api] = useState({test:"Hello Provider"})
+    const [web3Api, setweb3Api] = useState<Web3State>(createDefaultState())
+    const ethereum = typeof window !== 'undefined' ? window.ethereum : null;
+
+    const provider = ethereum ? new ethers.BrowserProvider(ethereum) : null;
+
+
+    useEffect(() => {
+      function initWeb3(){
+        setweb3Api({
+            ethereum : window.ethereum,
+            provider : provider,
+            contract : null,
+            isLoading : false
+        })
+      }
+      initWeb3()
+    }, [])
+    
     return (
 
         <Web3Context.Provider value={web3Api}>
--- nft/pages/index.tsx
+++ nft/pages/index.tsx
@@ -1,40 +1,58 @@
-
 /* eslint-disable @next/next/no-img-element */
 
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
-
-import nfts from "../content/meta.json"
-import { NftMeta } from '@/types/nft'
-import { BaseLayout } from '@/components/ui'
-import NftList from '@/components/ui/nfts/list'
-import { useweb3 } from '@/components/providers/web3'
-
+import nfts from "../content/meta.json";
+import { NftMeta } from "@/types/nft";
+import { BaseLayout } from "@/components/ui";
+import NftList from "@/components/ui/nfts/list";
+import { useweb3 } from "@/components/providers/web3";
+import { useEffect, useState } from "react";
+import { ethers,formatUnits} from "ethers";
 
 const Home: NextPage = () => {
+  const { ethereum, provider } = useweb3();
 
-  const {test} = useweb3()
+  const [accounts, setAccounts] = useState<string[] | null>(null);
+
+  useEffect(() => {
+    const fetchAccounts = async () => {
+      if (provider){
+        const res: string[] = await provider!.send("eth_requestAccounts", []);
+        console.log(res);
+        const signer = await provider.getSigner()
+        console.log("SIGNER -", signer)
+        console.log("ADDRESS ",signer.address)
+        const bal = provider.getBalance(signer.address)
+        console.log("BALANCE ",formatUnits(await bal,"ether"))
+        setAccounts(res);
+      }
+    };
+    fetchAccounts();
+  }, [provider]);
+
   return (
     <BaseLayout>
-    {test}
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
         <div className="absolute inset-0">
           <div className="bg-white h-1/3 sm:h-2/3" />
         </div>
         <div className="relative">
           <div className="text-center">
-            <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Amazing Creatures NFTs</h2>
+            <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+              Amazing Creatures NFTs
+            </h2>
             <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
               Mint a NFT to get unlimited ownership forever!
             </p>
           </div>
           <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-            <NftList item={nfts as unknown as NftMeta[]}/>
+            <NftList item={nfts as unknown as NftMeta[]} />
           </div>
         </div>
       </div>
     </BaseLayout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
